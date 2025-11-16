4caa7f58ee281f071abbba3485bee6065c19c8dd
宮内総一郎 <soichiromiyauchi@miyauchisouichirounoMacBook-Air.local>
2025-11-16 16:05:06 +0900

    Fix CORS for Vercel origin

--- backend/api.py
+++ backend/api.py
@@ -24,12 +24,14 @@ class RunPromptRequest(BaseModel):
 app = FastAPI()
 
 # CORS
+origins = [
+    "https://prompt-studio-khaki.vercel.app", 
+    "http://localhost:3000",                   
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://prompt-studio-khaki.vercel.app/",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
