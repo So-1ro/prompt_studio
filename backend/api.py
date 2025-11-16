244667d1b3ed5e2ba58e0899022af838abefc3eb
宮内総一郎 <soichiromiyauchi@miyauchisouichirounoMacBook-Air.local>
2025-11-16 16:45:27 +0900

    Update

--- backend/api.py
+++ backend/api.py
@@ -25,7 +25,7 @@ app = FastAPI()
 
 # CORS
 origins = [
-    "https://prompt-studio-khaki.vercel.app", 
+    "https://prompt-studio-iota.vercel.app/", 
     "http://localhost:3000",                   
 ]
 
